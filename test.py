e9d013e5845f2012fcf08a578ea72c054c198f49
GabrielEscobar04 <gaesca04@gmail.com>
2025-05-23 14:43:43 +0200

    Lint errors

--- test.py
+++ test.py
@@ -11,19 +11,17 @@ class TestStringMethods(unittest.TestCase):
 
     def test_is_upper(self):
         """Test that the to_upper_case function correctly converts a string to uppercase."""
-        string_val = transform.to_upper_case("hello") # Corrected variable name from sting to string_val
+        string_val = transform.to_upper_case("hello")
         self.assertEqual(string_val, "HELLO")
 
     def test_is_lower(self):
         """Test that the to_lower_case function correctly converts a string to lowercase."""
-        string_val = transform.to_lower_case("HELLO") # Corrected variable name from sting to string_val
-        self.assertEqual(string_val, "hello")
+        string_val = transform.to_lower_case("HELLO")
 
     def test_is_capitalize(self):
         """Test that the to_capitalize function correctly capitalizes a string."""
-        string_val = transform.to_capitalize("HELLO") # Corrected variable name from sting to string_val
+        string_val = transform.to_capitalize("HELLO")
         self.assertEqual(string_val, "Hello")
-
 
 if __name__ == '__main__':
     unittest.main()
